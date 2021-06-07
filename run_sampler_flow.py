05bff4e7dfa58106ed5598d6028e4f25681574a5
Bob Colner <bcolner@gmail.com>
2021-06-06 21:12:05 -0700

    renko_v3 fix

--- run_sampler_flow.py
+++ run_sampler_flow.py
@@ -3,7 +3,7 @@ from workflows import sampler_flow
 from workflows.configs import renko_v1, renko_v2
 
 
-config = renko_v2.config
+config = renko_v3.config
 
 prefix_data = '/data/trades'
 prefix_meta = f"/bars/{config['meta']['config_id']}/meta"
@@ -12,3 +12,5 @@ prefix_df = f"/bars/{config['meta']['config_id']}/df"
 ray.init(dashboard_host='0.0.0.0', dashboard_port=1111, ignore_reinit_error=True)
 
 bds = sampler_flow.run(config)
+
+ray.shutdown()
